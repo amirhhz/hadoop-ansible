458eca43bb25a10d8d81d44ae7cbbec9a16839b6
Amir H. Hajizamani <amir@rangespan.com>
2014-02-16 13:43:07 +0000

    Turn on ansible accelerate mode with an envvar, defaulting to off.
    So running script like this turns accelerate on:
      $ ACCEL=true ./site_alternative.sh

--- site_alternative.sh
+++ site_alternative.sh
@@ -6,11 +6,13 @@ export ANSIBLE_ERROR_ON_UNDEFINED_VARS=True
 export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_SSH_ARGS="-o ForwardAgent=yes"
 
+accelerate="${ACCEL:-false}"
+
 if [ $# -gt 0 ]
 then
   echo 'Running ansible-playbook -i hosts site_alternative.yml --tags' "$*"
-  ansible-playbook -i hosts --extra-vars "accelerate=false" site_alternative.yml --tags "$*"
+  ansible-playbook -i hosts --extra-vars "accelerate=$accelerate" site_alternative.yml --tags "$*"
 else
   echo 'Running ansible-playbook -i hosts site_alternative.yml'
-  ansible-playbook -i hosts --extra-vars "accelerate=false" site_alternative.yml
+  ansible-playbook -i hosts --extra-vars "accelerate=$accelerate" site_alternative.yml
 fi
